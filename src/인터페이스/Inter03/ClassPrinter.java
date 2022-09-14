dd8f5500ba730df03d163bcdec364e9db77d7eda
choidongkuen <danaver12@daum.net>
2022-09-14 22:02:19 +0900

    commit "09/14"

--- src/인터페이스/Inter03/ClassPrinter.java
+++ src/인터페이스/Inter03/ClassPrinter.java
@@ -1,4 +1,42 @@
+// System.out.println(Object obj) 는 해당 인스턴스의 toString 메소드 호출 !!
+
 package 인터페이스.Inter03;
 
+interface UpperCasePrintable{
+
+} // 비어있는 인터페이스
+
 public class ClassPrinter {
+    public static void print(Object obj){ // 정적 메소드
+        String org = obj.toString(); // 실제 인스턴스의 toString() 메소드 호출 # 메소드 오버라이딩
+        if(obj instanceof UpperCasePrintable) // UpperCasePrintable 로 형변환 가능하니? (Point(obj)) 일때, 가능
+            org = org.toUpperCase();
+
+        System.out.println(org);
+
+//        System.out.println(obj.toString()); // obj에 위치하는 인스턴스의 toString 메서드 호출
+
+    }
+}
+
+class Point implements UpperCasePrintable{ // 해당 인터페이스를 구현(상속)
+    private int xPos,yPos;
+
+    Point(int xPos, int yPos){
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+    public String toString(){
+        String posInfo = "[ x : " + this.xPos + ", y : " + this.yPos + " ]";
+        return posInfo;
+    }
+}
+
+class ImplObjectPrintln{
+    public static void main(String[] args) {
+        Point p1 = new Point(10,20);
+        Point p2 = new Point(12,8);
+        ClassPrinter.print(p1);
+        ClassPrinter.print(p2);
+    }
 }
---
+++ src/인터페이스/Inter05/MultiInteritance.java
@@ -0,0 +1,44 @@
+// 인터페이스와 클래스의 조합으로 다중상속 구현해보기
+
+package 인터페이스.Inter05;
+
+class TV{
+    public void tvOn(){
+        System.out.println("영상 출력 중");
+    }
+}
+
+interface Computer{
+    public void dataReceive(); // 추상 메소드
+}
+
+class ComputerImp {
+    public void dataReceive(){
+        System.out.println("영상 데이터 수신 중");
+    }
+}
+
+class IPTV extends TV implements Computer{
+
+    ComputerImp comp = new ComputerImp();
+
+    public void dataReceive(){ // 추상 메소드 구현
+        comp.dataReceive(); // ComputerImp 의 메소드 호출
+    }
+    public void powerOn(){
+        dataReceive(); // 클래스 내의 메소드 호출
+        tvOn(); // 상위 클래스의 메소드 호출
+    }
+}
+
+
+public class MultiInteritance {
+    public static void main(String[] args) {
+        IPTV iptv = new IPTV();
+        iptv.powerOn();
+
+        TV tv = iptv; // 업캐스팅
+        Computer computer = iptv; // 일종의 업캐스팅
+        System.out.println("hello".getClass());
+    }
+}
